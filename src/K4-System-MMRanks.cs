4d7f60c2b03f109e420e43ee362394bf64fd4c5a
K4ryuu <104531589+K4ryuu@users.noreply.github.com>
2024-03-25 13:48:58 +0100

    Initial Release

---
+++ src/K4-System-MMRanks.cs
@@ -0,0 +1,70 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes;
+using System.Text.Json.Serialization;
+using Microsoft.Extensions.Logging;
+using CounterStrikeSharp.API.Modules.Memory;
+using CounterStrikeSharp.API.Core.Capabilities;
+using K4SharedApi;
+
+namespace K4SystemMMRanks
+{
+	public sealed class PluginConfig : BasePluginConfig
+	{
+		[JsonPropertyName("mode")]
+		public int Mode { get; set; } = 1;
+
+		[JsonPropertyName("ConfigVersion")]
+		public override int Version { get; set; } = 1;
+	}
+
+	[MinimumApiVersion(198)]
+	public sealed partial class PluginK4SystemMMRanks : BasePlugin, IPluginConfig<PluginConfig>
+	{
+
+		public override string ModuleName => "K4-System Matchmaking Ranks";
+		public override string ModuleVersion => "1.0.0";
+		public override string ModuleAuthor => "K4ryuu";
+
+		public required PluginConfig Config { get; set; } = new PluginConfig();
+
+		public void OnConfigParsed(PluginConfig config)
+		{
+			if (config.Version < Config.Version)
+			{
+				base.Logger.LogWarning("Configuration version mismatch (Expected: {0} | Current: {1})", this.Config.Version, config.Version);
+			}
+
+			this.Config = config;
+		}
+
+		public static PlayerCapability<IK4SharedApi> Capability_SharedAPI { get; } = new("k4-system:sharedapi");
+
+		public override void Load(bool hotReload)
+		{
+			VirtualFunctions.CCSPlayerPawnBase_PostThinkFunc.Hook(_ =>
+			{
+				if (Config.Mode == 0)
+					return HookResult.Continue;
+
+				Utilities.GetPlayers().Where(p => p?.IsValid == true && p.PlayerPawn?.IsValid == true && !p.IsBot && !p.IsHLTV && p.SteamID.ToString().Length == 17)
+					.ToList()
+					.ForEach(p =>
+					{
+						IK4SharedApi? apiHandler = Capability_SharedAPI.Get(p);
+
+						if (apiHandler == null)
+							return;
+
+						int rankId = apiHandler.PlayerRankID;
+						int points = apiHandler.PlayerPoints;
+
+						p.CompetitiveRankType = (sbyte)(Config.Mode == 1 ? 11 : 12);
+						p.CompetitiveRanking = Config.Mode == 1 ? points : rankId >= 19 ? 18 : rankId;
+					});
+
+				return HookResult.Continue;
+			}, HookMode.Post);
+		}
+	}
+}
