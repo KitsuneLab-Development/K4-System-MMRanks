f8372c3978130de844b4c742337b9b891dd0070c
K4ryuu <104531589+K4ryuu@users.noreply.github.com>
2024-05-13 22:40:43 +0200

    Patch v1.0.4

--- src/K4-System-MMRanks.cs
+++ src/K4-System-MMRanks.cs
@@ -3,7 +3,6 @@ using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes;
 using System.Text.Json.Serialization;
 using Microsoft.Extensions.Logging;
-using CounterStrikeSharp.API.Modules.Memory;
 using CounterStrikeSharp.API.Core.Capabilities;
 using K4SharedApi;
 
@@ -18,22 +17,22 @@ namespace K4SystemMMRanks
 		public override int Version { get; set; } = 1;
 	}
 
-	[MinimumApiVersion(200)]
+	[MinimumApiVersion(227)]
 	public sealed partial class PluginK4SystemMMRanks : BasePlugin, IPluginConfig<PluginConfig>
 	{
 
 		public override string ModuleName => "K4-System Matchmaking Ranks";
-		public override string ModuleVersion => "1.0.3";
+		public override string ModuleVersion => "1.0.4";
 		public override string ModuleAuthor => "K4ryuu";
+
+		public bool AllowUpdate = false;
 
 		public required PluginConfig Config { get; set; } = new PluginConfig();
 
 		public void OnConfigParsed(PluginConfig config)
 		{
 			if (config.Version < Config.Version)
-			{
 				base.Logger.LogWarning("Configuration version mismatch (Expected: {0} | Current: {1})", this.Config.Version, config.Version);
-			}
 
 			this.Config = config;
 		}
@@ -42,30 +41,28 @@ namespace K4SystemMMRanks
 
 		public override void Load(bool hotReload)
 		{
+			if (hotReload)
+				AllowUpdate = true;
+
+			RegisterEventHandler((EventRoundStart @event, GameEventInfo info) => { AllowUpdate = true; return HookResult.Continue; });
+			RegisterEventHandler((EventCsWinPanelMatch @event, GameEventInfo info) => { AllowUpdate = false; return HookResult.Continue; });
+
 			RegisterListener<Listeners.OnTick>(() =>
-        	{
-				if (Config.Mode == 0)
+			{
+				if (Config.Mode == 0 || !AllowUpdate)
 					return;
 
-				Utilities.GetPlayers().Where(p => p?.IsValid == true && p.PlayerPawn?.IsValid == true && !p.IsBot && !p.IsHLTV && p.SteamID.ToString().Length == 17)
-				.ToList()
-				.ForEach(p =>
+				Utilities.GetPlayers().ForEach(p =>
 				{
-					IPlayerAPI? apiHandler = Capability_SharedAPI.Get(p);
-
-					if (apiHandler == null)
-						return;
-
-					if (!apiHandler.IsLoaded || !apiHandler.IsValid || !apiHandler.IsPlayer)
-						return;
-
-					int rankId = apiHandler.RankID;
-					int points = apiHandler.Points;
-
-					p.CompetitiveWins = 10;
-					p.CompetitiveRankType = (sbyte)(Config.Mode == 1 ? 11 : 12);
-					p.CompetitiveRanking = Config.Mode == 1 ? points : rankId >= 19 ? 18 : rankId;
-					Utilities.SetStateChanged(p, "CCSPlayerController", "m_iCompetitiveRankType");
+					IPlayerAPI? h = Capability_SharedAPI.Get(p);
+					if (h?.IsLoaded == true && h.IsValid && h.IsPlayer)
+					{
+						int rankId = h.RankID;
+						p.CompetitiveWins = 10;
+						p.CompetitiveRankType = (sbyte)(Config.Mode == 1 ? 11 : 12);
+						p.CompetitiveRanking = Config.Mode == 1 ? h.Points : rankId >= 19 ? 18 : rankId;
+						Utilities.SetStateChanged(p, "CCSPlayerController", "m_iCompetitiveRankType");
+					}
 				});
 			});
 		}
